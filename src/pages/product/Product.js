3cd8cfcb771fdfff9d1734b8c93eee4b257057eb
rangguy <ranggadwi100@gmail.com>
2024-11-17 22:27:25 +0700

    add:feat:import export excel products

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -2,15 +2,19 @@ import React, { useState, useEffect, useCallback } from "react";
 import Cookies from "js-cookie";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { FileUpload } from "primereact/fileupload";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Product = (props) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [file, setFile] = useState(null);
   const navigate = useNavigate();
 
   const fetchProducts = useCallback(async () => {
@@ -61,11 +65,107 @@ const Product = (props) => {
     }
   };
 
+  const handleExport = async () => {
+    try {
+      const token = Cookies.get("token");
+      const response = await axios.get(
+        "http://localhost:8080/api/products/export",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          responseType: "blob",
+        }
+      );
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "products.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("Error exporting products:", error);
+    }
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    if (showModal) {
+      setFile(null);
+    }
+  };
+
+  const handleImport = async () => {
+    if (!file) {
+      console.error("No file selected.");
+      return;
+    }
+
+    try {
+      const token = Cookies.get("token");
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await axios.post(
+        "http://localhost:8080/api/products/import",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      fetchProducts();
+      toggleModal();
+      setFile(null);
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: response.data.message,
+      });
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.error) {
+        const errorMessage = error.response.data.error;
+
+        Swal.fire({
+          icon: "error",
+          title: "Import Error",
+          text: errorMessage,
+        });
+
+        setFile(null);
+        toggleModal();
+        return;
+      }
+
+      console.error("Error importing products:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Something went wrong!",
+        text: "An unexpected error occurred. Please try again.",
+      });
+    }
+  };
+
+  const formatCurrency = (value) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    }).format(value);
+  };
+
+  const currencyBodyTemplate = (rowData, field) => {
+    return formatCurrency(rowData[field]);
+  };
+
   const actionBodyTemplate = (rowData) => {
     return (
       <div style={{ display: "flex", justifyContent: "center", gap: "8px" }}>
         <button
-          onClick={() => handleEdit(rowData.id)}
+          onClick={() => handleEdit(rowData)}
           className="btn btn-primary btn-sm d-flex align-items-center"
           title="Ubah"
         >
@@ -75,7 +175,7 @@ const Product = (props) => {
         <button
           onClick={() => handleDelete(rowData.id)}
           className="btn btn-danger btn-sm d-flex align-items-center"
-          title="Delete"
+          title="Hapus"
         >
           <i className="fas fa-trash"></i>
           <span>Hapus</span>
@@ -101,10 +201,113 @@ const Product = (props) => {
               </ol>
             </div>
           </div>
-          {/* Add Button */}
-          <Link to="/products/add" className="btn btn-success mb-3">
-            Tambah Produk
-          </Link>
+          <div className="card">
+            <div className="card-body">
+              <div
+                className="btn-group"
+                role="group"
+                aria-label="Product actions"
+              >
+                {/* Tombol Tambah Produk */}
+                <button
+                  className="btn btn-success"
+                  onClick={() => navigate("/products/add")}
+                >
+                  <i className="fas fa-plus me-2"></i>
+                  Tambah Produk
+                </button>
+
+                {/* Tombol Export Excel */}
+                <button className="btn btn-info" onClick={handleExport}>
+                  <i className="fas fa-download me-2"></i>
+                  Export Excel
+                </button>
+
+                {/* Tombol Import Excel */}
+                <div className="position-relative d-inline-block">
+                  <button className="btn btn-warning" onClick={toggleModal}>
+                    <i className="fas fa-upload me-2"></i>
+                    Import Excel
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Modal untuk Upload Excel */}
+            {showModal && (
+              <div
+                className="modal fade show"
+                style={{ display: "block", backgroundColor: "rgba(0,0,0,0.5)" }}
+                tabIndex="-1"
+                role="dialog"
+                aria-labelledby="modalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog" role="document">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="modalLabel">
+                        Upload File Excel
+                      </h5>
+                      <button
+                        type="button"
+                        className="close btn btn-danger"
+                        aria-label="Close"
+                        onClick={toggleModal}
+                      >
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <p>
+                        Hanya file dengan ekstensi .xlsx atau .xls yang
+                        diperbolehkan. Maksimal ukuran file adalah 1MB.
+                      </p>
+                      <FileUpload
+                        mode="basic"
+                        name="file"
+                        auto
+                        accept=".xlsx,.xls"
+                        maxFileSize={1000000}
+                        onSelect={(event) => setFile(event.files[0])} // Set the file on selection
+                        chooseLabel="Pilih File"
+                        className="w-100"
+                      />
+
+                      {file && (
+                        <div className="mt-3">
+                          <p>
+                            <strong>Nama File:</strong> {file.name}
+                          </p>
+                          <p>
+                            <strong>Ukuran File:</strong>{" "}
+                            {(file.size / 1024).toFixed(2)} KB
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-secondary"
+                        onClick={toggleModal} // Cancel and clear the file
+                      >
+                        Batal
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={handleImport}
+                        disabled={!file} // Disable the button if no file is selected
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
       <section className="content">
@@ -118,12 +321,32 @@ const Product = (props) => {
             loading={loading}
             emptyMessage="Data produk masih kosong"
           >
-            <Column field="name" header="Name" sortable />
-            <Column field="category_name" header="Category" sortable />
-            <Column field="net_profit" header="Keuntungan Bersih" sortable />
-            <Column field="gross_profit" header="Keuntungan Kotor" sortable />
-            <Column field="gross_sale" header="Penjualan Kotor" sortable />
-            <Column field="purchase_cost" header="Harga Beli" sortable />
+            <Column field="name" header="Nama Produk" sortable />
+            <Column field="category_name" header="Kategori" sortable />
+            <Column
+              field="net_profit"
+              header="Keuntungan Bersih"
+              sortable
+              body={(rowData) => currencyBodyTemplate(rowData, "net_profit")}
+            />
+            <Column
+              field="gross_profit"
+              header="Keuntungan Kotor"
+              sortable
+              body={(rowData) => currencyBodyTemplate(rowData, "gross_profit")}
+            />
+            <Column
+              field="gross_sale"
+              header="Penjualan Kotor"
+              sortable
+              body={(rowData) => currencyBodyTemplate(rowData, "gross_sale")}
+            />
+            <Column
+              field="purchase_cost"
+              header="Harga Beli"
+              sortable
+              body={(rowData) => currencyBodyTemplate(rowData, "purchase_cost")}
+            />
             <Column field="initial_stock" header="Stok Awal" sortable />
             <Column field="final_stock" header="Stok Akhir" sortable />
             <Column
